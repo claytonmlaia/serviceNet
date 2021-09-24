c4606a3853d1a5563edc8a66fbc1755d0f50a143
Clayton Martins Laia <claytonmlaia@hotmail.com>
2021-09-23 22:04:02 -0300

    Migration
    Criação da migration referente a tabela de clientes.

---
+++ database/migrations/2021_09_24_005735_create_clientes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('telefone');
+            $table->string('endereco');
+            $table->string('numero');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('pais');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
